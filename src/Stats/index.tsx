81c7624639cad8f5da9a1614cb2e820482c4d89b
Divine Olokor <divee789@gmail.com>
2020-06-24 03:21:10 +0100

    feat: final prototype, deploying to production

--- src/Stats/index.tsx
+++ src/Stats/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
 import Request from "../API/covid19";
 
@@ -9,8 +9,10 @@ import "./index.scss";
 
 const API = new Request();
 
-const Stats = () => {
+const Stats = (props) => {
+  const [loading, setLoading] = useState(false);
   const [statistics, setStatistics] = useState([]);
+  const [statisticsA, setStatisticsA] = useState([]);
   const [worldStats, setWorldStats] = useState({
     TotalConfirmed: "",
     TotalDeaths: "",
@@ -20,12 +22,16 @@ const Stats = () => {
   useEffect(() => {
     const getData = async () => {
       try {
+        setLoading(true);
         const worldData = await API.getWorldStats();
         const data = await API.getAllCountriesStats();
+        setLoading(false);
         setWorldStats(worldData);
         setStatistics(data);
+        setStatisticsA(data);
       } catch (error) {
-        console.log(error);
+        alert("There has been an error getting the coronavirus data");
+        setLoading(false);
       }
     };
     getData();
@@ -34,7 +40,12 @@ const Stats = () => {
   return (
     <header>
       <nav>
-        <div className="logo_image">
+        <div
+          className="logo_image"
+          onClick={() => {
+            props.history.push("/");
+          }}
+        >
           <img src={logo} alt="logo" />
         </div>
         <div className="nav_items">
@@ -43,103 +54,171 @@ const Stats = () => {
           </Link>
         </div>
       </nav>
-      <div className="total_section">
-        <div>
-          <p>Total Cases</p>
-          <p>
-            {worldStats.TotalConfirmed.toString().replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ",",
-            )}
-          </p>
-        </div>
-        <div>
-          <p>Total Recovered</p>
-          <p>
-            {worldStats.TotalRecovered.toString().replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ",",
-            )}
-          </p>
-        </div>
-        <div>
-          <p>Total Deaths</p>
-          <p>
-            {worldStats.TotalDeaths.toString().replace(
-              /\B(?=(\d{3})+(?!\d))/g,
-              ",",
-            )}
-          </p>
-        </div>
-      </div>
-      <div className="data_section">
-        {statistics.map((stat: any) => {
-          return (
-            <div className="card">
-              <img src={stat.countryInfo.flag} alt="country flag" />
-              <div>
-                <div>
-                  <p>Country</p>
-                  <p>{stat.country}</p>
-                </div>
-                <div>
-                  <p>Continent</p>
-                  <p>{stat.continent}</p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <p>Total Cases</p>
-                  <p>{stat.cases}</p>
-                </div>
-                <div>
-                  <p>Critical Cases</p>
-                  <p>{stat.critical}</p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <p>Population</p>
-                  <p>{stat.population}</p>
-                </div>
-                <div>
-                  <p>Total Deaths</p>
-                  <p>{stat.deaths}</p>
-                </div>
-              </div>
-
-              <div>
-                <div>
-                  <p>Total Recovered</p>
-                  <p>{stat.recovered}</p>
-                </div>
-                <div>
-                  <p>Total tests</p>
-                  <p>{stat.tests}</p>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <p>Today Cases</p>
-                  <p>{stat.todayCases}</p>
-                </div>
-                <div>
-                  <p>Today Deaths</p>
-                  <p>{stat.todayDeaths}</p>
-                </div>
-                <div>
-                  <p>Today Recovered</p>
-                  <p>{stat.todayRecovered}</p>
-                </div>
-              </div>
+      {loading && (
+        <p style={{ textAlign: "center", marginTop: "15%" }}>
+          Loading Data....
+        </p>
+      )}
+      {!loading && (
+        <>
+          <div className="total_section">
+            <div>
+              <p>Total Cases</p>
+              <p>
+                {worldStats.TotalConfirmed.toString().replace(
+                  /\B(?=(\d{3})+(?!\d))/g,
+                  ",",
+                )}
+              </p>
             </div>
-          );
-        })}
-      </div>
+            <div>
+              <p>Total Recovered</p>
+              <p>
+                {worldStats.TotalRecovered.toString().replace(
+                  /\B(?=(\d{3})+(?!\d))/g,
+                  ",",
+                )}
+              </p>
+            </div>
+            <div>
+              <p>Total Deaths</p>
+              <p>
+                {worldStats.TotalDeaths.toString().replace(
+                  /\B(?=(\d{3})+(?!\d))/g,
+                  ",",
+                )}
+              </p>
+            </div>
+          </div>
+          <div className="search_container">
+            <input
+              type="text"
+              placeholder="Search for a Country by Name"
+              onChange={(e) => {
+                if (e.target.value === "") {
+                  setStatistics(statisticsA);
+                } else {
+                  const filterByFirstLetterStatistics = statistics.filter(
+                    (stat: any) =>
+                      stat.country
+                        .toLowerCase()
+                        .startsWith(e.target.value.toLowerCase()),
+                  );
+                  setStatistics(filterByFirstLetterStatistics);
+                }
+              }}
+            />
+          </div>
+          <div className="data_section">
+            {statistics.map((stat: any) => {
+              return (
+                <div className="card">
+                  <img src={stat.countryInfo.flag} alt="country flag" />
+                  <div className="card_info">
+                    <div>
+                      <p>Country</p>
+                      <p>{stat.country}</p>
+                    </div>
+                    <div>
+                      <p>Continent</p>
+                      <p>{stat.continent}</p>
+                    </div>
+                  </div>
+
+                  <div className="card_info">
+                    <div>
+                      <p>Total Cases</p>
+                      <p>
+                        {stat.cases
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                    <div>
+                      <p>Critical Cases</p>
+                      <p>
+                        {stat.critical
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="card_info">
+                    <div>
+                      <p>Population</p>
+                      <p>
+                        {stat.population
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                    <div>
+                      <p>Total Deaths</p>
+                      <p>
+                        {stat.deaths
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="card_info">
+                    <div>
+                      <p>Total Recovered</p>
+                      <p>
+                        {stat.recovered
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                    <div>
+                      <p>Total tests</p>
+                      <p>
+                        {stat.tests
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="card_info">
+                    <div>
+                      <p>Today Cases</p>
+                      <p>
+                        {stat.todayCases
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                    <div>
+                      <p>Today Deaths</p>
+                      <p>
+                        {stat.todayDeaths
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                  </div>
+
+                  <div className="card_info">
+                    <div>
+                      <p>Today Recovered</p>
+                      <p>
+                        {stat.todayRecovered
+                          .toString()
+                          .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
     </header>
   );
 };
 
-export default Stats;
+export default withRouter(Stats);
